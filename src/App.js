b2751b134881d35dad20680d21618a7f3804c3b8
leif.zhang <leif.zhang@meetsocial.cn>
2017-12-04 15:47:46 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(prop){
+      super()
+      this.state = {
+          num :0
+      }
+    }
+
+    addTime(){
+        let temp = 0
+        setInterval(() => {
+            temp++
+            this.setState({
+                num:temp
+            })
+        },500)
+    }
+
+    componentDidMount(){
+        this.addTime()
+    }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">庆祝第一次成功,哈哈哈</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reloaddddd.
+
         </p>
+          <p>{this.state.num}</p>
       </div>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AppContainer } from 'react-hot-loader'
-import registerServiceWorker from './registerServiceWorker';
 const render = Component => {
     ReactDOM.render(
         <AppContainer>
